30a8675b4a494b266c3b68cfdca9fa1c972617b2
Travis Southard <tsouthard88@gmail.com>
2020-07-24 16:13:42 -0400

    Took handleChange and handleSubmit from TransactionForm and put them in App.js with update values to align data

--- xpense_front_end/src/App.js
+++ xpense_front_end/src/App.js
@@ -27,46 +27,51 @@ class App extends React.Component {
         budget: 10,
         spent: 6,
         transactions: [],
-    },
-    {
+      },
+      {
         title: "Food",
         budget: 0,
         spent: 0,
         transactions: [],
-    },
-    {
+      },
+      {
         title: "Lodging",
         budget: 0,
         spent: 0,
         transactions: [],
-    },
-    {
+      },
+      {
         title: "Entertainment",
         budget: 0,
         spent: 0,
         transactions: [],
-    },
-    {
+      },
+      {
         title: "Shopping",
         budget: 0,
         spent: 0,
         transactions: [],
-    },
-    {
+      },
+      {
         title: "Car rental",
         budget: 0,
         spent: 0,
         transactions: [],
-    },
-    {
+      },
+      {
         title: "Misc.",
         budget: 0,
         spent: 0,
         transactions: [],
-    }]
+      }],
+      transactionForm: {
+        date: "",
+        payee: "",
+        category: "",
+        spent: 0,
+      },
     }
   }
-
 
   getBudget = () => {
     fetch(baseUrl + '/').then(res => {
@@ -91,11 +96,43 @@ class App extends React.Component {
     this.getBudget();
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(this.props.baseUrl + "/budgets/" + this.state.category, {
+      method: "PUT",
+      body: JSON.stringify({
+        transactions: [{
+          date: this.state.date,
+          payee: this.state.payee,
+          category: this.state.category,
+          spent: this.state.spent,
+        }],
+      }).then(res => res.json(
+      )).then(data => {
+        const copyBudgets = [...this.state.budget];
+        const findIndex = this.state.budget.findIndex(budget => budget.category === data.category);
+        copyBudgets[findIndex].transactions.push(data.transactions);
+        this.setState({budget: copyBudgets});
+      })
+  }
+
   render() {
     return (
       <div>
         <h1>Xpense App</h1>
-        <TransactionForm baseUrl={baseUrl} budget={this.state.budget}/>
+        <TransactionForm
+          baseUrl={baseUrl}
+          budget={this.state.budget}
+          transactionForm={this.state.transactionForm}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
         <BudgetTable budget={this.state.budget} />
       </div>
     )
--- xpense_front_end/src/components/TransactionForm.jsx
+++ xpense_front_end/src/components/TransactionForm.jsx
@@ -1,57 +1,26 @@
 import React, { Component } from 'react'
 
 export default class TransactionForm extends Component {
-    state = {
-        date: "",
-        payee: "",
-        category: "",
-        spent: 0,
-    }
-    handleChange = (event) => {
-        this.setState({
-            [event.target.id]: event.target.value,
-        })
-    }
-    handleSubmit = (event) => {
-        event.preventDefault();
-        fetch(this.props.baseUrl + "/budgets/" + this.state.category, {
-            method: "PUT",
-            body: JSON.stringify({
-                transactions: [{
-                    date: this.state.date,
-                    payee: this.state.payee,
-                    category: this.state.category,
-                    spent: this.state.spent,
-            }],
-            }).then(res => res.json(
-            )).then(data => {
-                const copyBudgets = [...this.props.budget];
-                const findIndex = this.props.budget.findIndex(budget => budget.category === data.category);
-                copyBudgets[findIndex].transactions.push(data.transactions);
-                // this.setState({budget: copyBudgets}) // Would I need to put this in app.js and prop it down to set State in the App.js and if so, how do i give the category as a parameter then with the event param
-            })
-        })
-    }
     render() {
         return (
             <form>
                 <label htmlFor="date">Date: 
                     <input
-                        onChange={(event) => this.handleChange(event)}
+                        onChange={(event) => this.props.handleChange(event)}
                         type="date"
                         id="date"
                         name="date"
-                        value={this.state.date}
+                        value={this.props.transactionForm.date}
                     />
                 </label>
                 <label htmlFor="payee">Payee: 
-                    <input onChange={(event) => this.handleChange(event)} type="text" id="payee" name="payee" value={this.state.payee}/>
+                    <input onChange={(event) => this.props.handleChange(event)} type="text" id="payee" name="payee" value={this.props.transactionForm.payee}/>
                 </label>
                 <label htmlFor="category">Category: 
-                    <input onChange={(event) => this.handleChange(event)} type="text" id="category" name="category" value={this.state.category}/>
+                    <input onChange={(event) => this.props.handleChange(event)} type="text" id="category" name="category" value={this.props.transactionForm.category}/>
                 </label>
                 <label htmlFor="spent">Spent: 
-                    <input onChange={(event) => this.handleChange(event)} type="number" id="spent" name="spent" value={this.state.spent}/>
+                    <input onChange={(event) => this.props.handleChange(event)} type="number" id="spent" name="spent" value={this.props.transactionForm.spent}/>
                 </label>
                 <input type="submit" value="Add Transaction" />
             </form>
