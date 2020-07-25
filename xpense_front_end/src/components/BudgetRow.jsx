229d9901c3748f9d5ac3a8d7868b84487210e739
Travis Southard <tsouthard88@gmail.com>
2020-07-25 13:00:38 -0400

    Added logic to hide the transaction modal in the BudgetRow

--- xpense_front_end/src/components/BudgetRow.jsx
+++ xpense_front_end/src/components/BudgetRow.jsx
@@ -4,12 +4,27 @@ import TransactionModal from './TransactionModal'
 
 
 class BudgetRow extends Component {
+  state = {
+    transactionModalOn: false,
+  }
+  toggleTransactionModal = () => {
+    this.setState({
+      transactionModalOn: !this.state.transactionModalOn,
+    })
+  }
   render () {
     return (
       <tr>
         <td>{this.props.title}</td>
         <td><BudgetInput budget={this.props.budget}/></td>
-        <td>{this.props.spent}<TransactionModal transactions={this.props.transactions}/></td>
+        <td onClick={() => this.toggleTransactionModal()}>
+          {this.props.spent}
+          {this.state.transactionModalOn ? (
+            <TransactionModal transactions={this.props.transactions}/>
+          ) : (
+            ""
+          )}
+        </td>
         <td>{this.props.budget-this.props.spent}</td>
       </tr>
     )
