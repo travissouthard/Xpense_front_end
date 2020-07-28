5d8ed4b3411cc2278e5a39b90724c003c6292c37
Tania Sainvilus <taniasainvilus@Tanias-Air.hsd1.ga.comcast.net>
2020-07-27 20:28:31 -0400

    removed test data

--- src/App.js
+++ src/App.js
@@ -19,48 +19,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      budget:   [{
-        title: "Gas",
-        budget: 10,
-        spent: 6,
-        transactions: [],
-    },
-    {
-        title: "Food",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Lodging",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Entertainment",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Shopping",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Car rental",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    },
-    {
-        title: "Misc.",
-        budget: 0,
-        spent: 0,
-        transactions: [],
-    }],
+      budget:   [],
       date: "",
       payee: "",
       category: "",
